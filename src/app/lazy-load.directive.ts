4a4f858065b4ab8dc599dbea67764a04b8b028eb
megarohas <a.galkin@desoft.pro>
2019-12-28 23:52:02 +0600

    img onerror was added

--- src/app/lazy-load.directive.ts
+++ src/app/lazy-load.directive.ts
@@ -7,34 +7,39 @@ export class LazyLoadDirective {
   @Input() srcdata: string;
   el: ElementRef;
   observer: Object;
+  loading_img: string;
+  error_img: string;
 
   loadImg(el: ElementRef) {
-    let actual_src = this.srcdata;
-    let observer = this.observer;
+    let { srcdata, observer, error_img } = this;
 
-    el.nativeElement.src = actual_src;
-    observer.unobserve(el.nativeElement);
+    el.nativeElement.src = srcdata;
+    el.nativeElement.onerror = () => {
+      el.nativeElement.src = error_img;
+    };
+    if (observer) observer.unobserve(el.nativeElement);
   }
 
   constructor(el: ElementRef) {
     this.el = el;
+    this.error_img = "https://clck.ru/LaNDr";
+    this.loading_img = "https://clck.ru/LaQ32";
   }
 
   ngOnInit() {
-    let el = this.el;
-    let observer = this.observer;
-    el.nativeElement.src = "https://clck.ru/LaADc";
+    let { el, observer, loading_img } = this;
+    let intersection_observer_config = {
+      rootMargin: "100px"
+    };
 
-    observer = new IntersectionObserver(
-      (entries, obs) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.loadImg(el);
-          }
-        });
-      },
-      { rootMargin: "100px" }
-    );
+    el.nativeElement.src = loading_img;
+    observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.loadImg(el);
+        }
+      });
+    }, intersection_observer_config);
     observer.observe(el.nativeElement);
   }
 }
