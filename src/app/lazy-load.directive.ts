a3d152359dea68656faedc94e039d355d4b35afb
megarohas <a.galkin@desoft.pro>
2019-12-28 19:38:42 +0600

    refactoring

--- src/app/lazy-load.directive.ts
+++ src/app/lazy-load.directive.ts
@@ -4,16 +4,22 @@ import { Directive, ElementRef, Input } from "@angular/core";
   selector: "[appLazyLoad]"
 })
 export class LazyLoadDirective {
+  @Input() srcdata: string;
+  el: ElementRef;
+
   constructor(el: ElementRef) {
-    let actual_src = el.nativeElement.getAttribute("srcdata");
+    this.el = el;
+  }
+
+  ngOnInit() {
+    let el = this.el;
+    let actual_src = this.srcdata;
     el.nativeElement.src =
       "https://static-steelkiwi-dev.s3.amazonaws.com/media/filer_public/1c/30/1c302b65-8285-4934-8917-7165523e766b/9382e6fa-5df6-433b-a4f3-4ed97bbeaf37.gif";
 
     let observer = new IntersectionObserver((entries, obs) => {
-      console.log(entries.length);
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          console.log("entry:", entry.isIntersecting);
           el.nativeElement.src = actual_src;
           observer.unobserve(el.nativeElement);
         }
